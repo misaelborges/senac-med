b1e281f832a265a9fd497fa33be7b46892edf0de
murialdopagani <murialdo.pagani@gmail.com>
2025-06-30 14:04:40 -0300

    Criado classe EspecialidadeDAO

---
+++ src/main/java/com/projeto/senac/med/dao/EspecialidadeDAO.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.projeto.senac.med.dao;
+
+import com.projeto.senac.med.util.Conexao;
+
+/**
+ *
+ * @author Murialdo
+ */
+public class EspecialidadeDAO {
+    
+}
