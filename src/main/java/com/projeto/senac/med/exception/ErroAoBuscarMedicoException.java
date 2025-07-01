f9a28feb2759de0ded2e7bdfb172ea0aac62fe33
Mizael <mizaelborges2011@gmail.com>
2025-07-01 19:15:16 -0300

    adicina ErroAoBuscarMedicaException e ErroAoSalvarException

---
+++ src/main/java/com/projeto/senac/med/exception/ErroAoBuscarMedicoException.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.projeto.senac.med.exception;
+
+/**
+ *
+ * @author mizael
+ */
+public class ErroAoBuscarMedicoException extends NegocioException{
+
+    public ErroAoBuscarMedicoException(String message) {
+        super(message);
+    }
+
+    public ErroAoBuscarMedicoException(String message, Throwable cause) {
+        super(message, cause);
+    }
+    
+    
+    
+}
